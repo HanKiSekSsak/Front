bfbdd9259361d04f8b7737e2aecf600d197254a5
Janghun Lee <janghunlee@Janghunui-MacBookAir.local>
2022-07-06 17:07:16 +0900

    Merge Frontend File

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 // For git commit
+let ingredientBtn = document.getElementById('ingredient-btn');
+let categoryBtn = document.getElementById('category-btn');
+
+
+let IngredientMove = ()=>{
+    location.href = "/ingredient";
+}
+
+let CategoryMove = ()=>{
+    location.href = "/category";
+}
+
+window.onload = ()=>{
+    ingredientBtn.addEventListener("click", IngredientMove);
+    categoryBtn.addEventListener("click", CategoryMove);
+}
