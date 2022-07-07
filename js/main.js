4a1f65bc8f702dc201f907e6dc831b783530329a
Jhun24 <jjanghuni102@naver.com>
2022-07-07 15:29:03 +0900

    Edit barcode.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // For git commit
 let ingredientBtn = document.getElementById('ingredient-btn');
 let categoryBtn = document.getElementById('category-btn');
+let barcodeBtn = document.getElementById('barcode-btn');
 
 
 let IngredientMove = ()=>{
@@ -11,7 +12,12 @@ let CategoryMove = ()=>{
     location.href = "/category";
 }
 
+let BarcodeMove = ()=>{
+    location.href = "/barcode";
+}
+
 window.onload = ()=>{
     ingredientBtn.addEventListener("click", IngredientMove);
     categoryBtn.addEventListener("click", CategoryMove);
+    barcodeBtn.addEventListener("click", BarcodeMove);
 }
